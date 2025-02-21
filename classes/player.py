3b1b2ec95a3d66fc249b1dc692d8b74ec38d1da6
gempocket <tomasnikkhah@gmail.com>
2025-02-21 12:24:11 +0100

    Update player.py

--- classes/player.py
+++ classes/player.py
@@ -1,6 +1,24 @@
 
 import pygame
 
+
+player_walk = [
+    pygame.image.load("assets/walk_1.png"),
+    pygame.image.load("assets/walk_2.png"),
+    pygame.image.load("assets/walk_3.png")
+]
+
+player_jump = [
+    pygame.image.load("assets/jump_1.png"),
+    pygame.image.load("assets/jump_2.png")
+]
+
+player_crouch = [
+    pygame.image.load("assets/crouch_1.png"),
+    pygame.image.load("assets/crouch_2.png")
+]
+
+
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
