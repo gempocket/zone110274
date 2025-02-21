8d6dc167968085effc15048af32f0a8549f9d200
gempocket <tomasnikkhah@gmail.com>
2025-02-21 12:46:40 +0100

    Update and rename intepret_keys.py to handle_events_and_keys.py

---
+++ functions/handle_events_and_keys.py
@@ -0,0 +1,17 @@
+
+def handle_events_and_keys(events, keys):
+    for event in events:
+        if event.type == KEYUP:
+            # Handle KEYUP events here
+            pass
+        elif event.type == KEYDOWN:
+            # Handle KEYDOWN events here
+            pass
+    
+    if keys[pygame.K_a]:
+        # Action for key 'a' being pressed
+        pass
+    
+    if keys[pygame.K_d]:
+        # Action for key 'd' being pressed
+        pass
