da9015a6a61d344be1c25fc61fb49b9e59efe748
gempocket <tomasnikkhah@gmail.com>
2025-02-21 12:26:07 +0100

    Update player.py

--- classes/player.py
+++ classes/player.py
@@ -3,19 +3,19 @@ import pygame
 
 
 player_walk = [
-    pygame.image.load("assets/walk_1.png"),
-    pygame.image.load("assets/walk_2.png"),
-    pygame.image.load("assets/walk_3.png")
+    pygame.image.load("assets/player_walk_1.png").convert(),
+    pygame.image.load("assets/player_walk_2.png").convert(),
+    pygame.image.load("assets/player_walk_3.png").convert()
 ]
 
 player_jump = [
-    pygame.image.load("assets/jump_1.png"),
-    pygame.image.load("assets/jump_2.png")
+    pygame.image.load("assets/player_jump_1.png").convert(),
+    pygame.image.load("assets/player_jump_2.png").convert()
 ]
 
 player_crouch = [
-    pygame.image.load("assets/crouch_1.png"),
-    pygame.image.load("assets/crouch_2.png")
+    pygame.image.load("assets/player_crouch_1.png").convert(),
+    pygame.image.load("assets/player_crouch_2.png").convert()
 ]
 
 
